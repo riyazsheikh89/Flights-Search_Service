1bc48be3f7ec0f6e4741034bf2791383cc838d6a
Sheikh Riyaz Ahmed <wbriyaz89@gmail.com>
2022-12-22 19:59:46 +0530

    Completed City Repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,12 +1,15 @@
 const { City } = require('../models/index');
 
 class CityRepository {
+
+    // this func expect a object as parameter, 
+    // in JS we can pass the properties of the object that we need, using {}
     async createCity ({name}) {
         try{
             const city = await City.create({name});
             return city;
-
         } catch (error) {
+            console.log("something went wrong in repository layer!");
             throw {error};
         }
     }
@@ -14,13 +17,35 @@ class CityRepository {
     async deleteCity(cityID) {
         try{
             await City.destroy({
-                where: {
-                    id: cityID
-                }
+                where: { id: cityID }
             });
         } catch(error){
+            console.log("something went wrong in repository layer!");
             throw {error};
         }
+    }
+
+    async getCity(cityID) {
+        try {
+            const city = await City.findBypk(cityID);
+            return city;
+        } catch (error) {
+            console.log("something went wrong in repository layer!");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityID, data) {
+        try {
+            const city = await City.update(data, {
+                where: {id: cityID}
+            });
+            return city;
+        } catch (error) {
+            console.log("something went wrong in repository layer!");
+            throw {error};
+        }
+
     }
 }
 
